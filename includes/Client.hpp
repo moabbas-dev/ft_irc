8da434b09e287924110390b10931e8bf58265cdf
ahmadfa-lb <farachiahmad2001@hotmail.com>
2024-12-27 15:15:08 +0200

    adding getters/setters for Client

--- includes/Client.hpp
+++ includes/Client.hpp
@@ -17,11 +17,20 @@ private:
 
 public:
     Client();
+    // Getters
     int getFd() const;
     std::string getNickname() const;
-    void setFd(int fd);
+    std::string getUsername() const;
     std::string getIPadd() const;
+    bool getIsAuthenticated() const;
+
+    // Setters
+    void setFd(int fd);
+    void setNickname(const std::string& nickname);
+    void setUsername(const std::string& username);
     void setIPadd(const std::string& IPadd);
+    void setIsAuthenticated(bool isAuthenticated);
+
     bool isOperator(const std::string& channel) const;
     void joinChannel(const std::string& channel);
     void leaveChannel(const std::string& channel);
--- srcs/Client.cpp
+++ srcs/Client.cpp
@@ -3,25 +3,50 @@
 Client::Client() : fd(-1), isAuthenticated(false) {
 }
 
+// Getters
+
 int Client::getFd() const {
     return fd;
-}
-
-void Client::setFd(int fd) {
-    this->fd = fd;
 }
 
 std::string Client::getNickname() const {
     return nickname;
 }
 
+std::string Client::getUsername() const {
+    return username;
+}
+
 std::string Client::getIPadd() const {
     return IPadd;
+}
+
+bool Client::getIsAuthenticated() const {
+    return isAuthenticated;
+}
+
+// Setters
+
+void Client::setFd(int fd) {
+    this->fd = fd;
+}
+
+void Client::setNickname(const std::string& nickname) {
+    this->nickname = nickname;
+}
+
+void Client::setUsername(const std::string& username) {
+    this->username = username;
 }
 
 void Client::setIPadd(const std::string& IPadd) {
     this->IPadd = IPadd;
 }
+
+void Client::setIsAuthenticated(bool isAuthenticated) {
+    this->isAuthenticated = isAuthenticated;
+}
+
 
 bool Client::isOperator(const std::string& channel) const {
     for (std::vector<std::string>::const_iterator it = channels.begin(); it != channels.end(); ++it)
