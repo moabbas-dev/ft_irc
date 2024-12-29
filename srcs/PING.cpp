818dad041023a3ae76e32f4a50f614d7611053c7
ahmadfa-lb <farachiahmad2001@hotmail.com>
2024-12-29 23:24:56 +0200

    implementing the PING cmd - will be edited later

--- srcs/PING.cpp
+++ srcs/PING.cpp
@@ -6,16 +6,24 @@
 /*   By: afarachi <afarachi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/27 23:49:35 by afarachi          #+#    #+#             */
-/*   Updated: 2024/12/28 14:54:56 by afarachi         ###   ########.fr       */
+/*   Updated: 2024/12/29 23:20:21 by afarachi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Cmd.hpp"
 
-void Cmd::PING(const Cmd& cmd, Server& server, Client& client) {
+void Cmd::PING(const Cmd &cmd, Server &server, Client &client)
+{
     std::cout << "Executing PING command\n";
-    (void)cmd;
+    // (void)cmd;
     (void)server;
-    (void)client;
-    // @mabbas && @jfatfat > here we can  implement PING
+    // (void)client;
+    if (cmd.getParams().empty() || !cmd.getParams().size())
+        return (cmd.errorServerClient("409", "empty ....", client.getFd()));
+    std::string	_string_to_send = ":" + client.getUsername() + " PONG :" + cmd.getParams()[0] + "\n";
+    send(client.getFd(), _string_to_send.c_str(), _string_to_send.length(), 0);
 }
+
+// a cmd like this PING LOL
+// should return on the client terminal like this 
+// :afarachi!afarachi@localhost PONG :LOL
